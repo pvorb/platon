93368ee650305929c6453f5632f70fea7ee9d098
Paul Vorbach <paul@vorba.ch>
2016-10-12 00:41:22 +0200

    Show more comment details and replies

--- src/main/javascript/components/comment/index.js
+++ src/main/javascript/components/comment/index.js
@@ -1,6 +1,9 @@
 var template = require('./comment.html');
 
+var dateToString = require('./date-to-string.js');
+
 module.exports = {
+    name: 'platon-comment',
     props: {
         comment: {
             type: Object,
@@ -8,5 +11,19 @@ module.exports = {
         }
     },
     render: template.render,
-    staticRenderFns: template.staticRenderFns
+    staticRenderFns: template.staticRenderFns,
+    data: function () {
+        return {
+            showReplyForm: false,
+            commentDraft: {}
+        }
+    },
+    computed: {
+        creationDate: function () {
+            return dateToString(new Date(this.comment.creationDate));
+        }
+    },
+    components: {
+        'platon-comment-form': require('../comment-form')
+    }
 };
