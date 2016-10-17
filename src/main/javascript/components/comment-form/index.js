138a6582e4b7840c42fcf9df305e1537362b556f
Paul Vorbach <paul@vorba.ch>
2016-10-17 23:24:21 +0200

    Add markdown preview

--- src/main/javascript/components/comment-form/index.js
+++ src/main/javascript/components/comment-form/index.js
@@ -1,3 +1,12 @@
+var debounce = require('lodash.debounce');
+var marked = require('marked');
+marked.setOptions({
+    gfm: false,
+    sanitize: true,
+    smartLists: true,
+    smartypants: true
+});
+
 var UserInfoService = require('./../../services/user-info-service.js');
 var CommentService = require('../../services/comment-service.js');
 
@@ -17,11 +26,25 @@ module.exports = {
     data: function () {
         return {
             comment: UserInfoService.getUserInfo(),
-            rememberUser: UserInfoService.getRememberUser()
+            rememberUser: UserInfoService.getRememberUser(),
+            showPreview: false,
+            previewStyle: {},
+            markdown: ''
         };
     },
 
     methods: {
+        togglePreview: function () {
+            this.showPreview = !this.showPreview;
+            if (this.showPreview) {
+                this.comment.text = marked(this.markdown);
+            }
+            try {
+                this.previewStyle.height = this.$el.getElementsByClassName('platon-form-text')[0].offsetHeight;
+            } catch (e) {
+                console.error(e.message);
+            }
+        },
         postComment: function () {
             var vm = this;
 
@@ -36,6 +59,7 @@ module.exports = {
             }
 
             vm.comment.parentId = vm.parentId;
+            vm.comment.text = marked(vm.markdown);
 
             CommentService.postComment(window.location.pathname, document.title, vm.comment)
                 .then(function (newComment) {
