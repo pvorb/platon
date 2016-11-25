5018a1f40f3cf3475766bdfced0ffb3e1813b2c9
Paul Vorbach <paul@vorba.ch>
2016-11-25 20:39:55 +0100

    Test input sanitizer configuration

--- src/main/java/de/vorb/platon/PlatonApp.java
+++ src/main/java/de/vorb/platon/PlatonApp.java
@@ -25,6 +25,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -36,48 +37,8 @@ import java.time.Instant;
 @SpringBootApplication
 public class PlatonApp {
 
-    private static final Logger logger = LoggerFactory.getLogger(PlatonApp.class);
-
     public static void main(String... args) {
         SpringApplication.run(PlatonApp.class, args);
-    }
-
-    public PlatonApp() throws NoSuchAlgorithmException {
-    }
-
-    @Bean
-    public WebMvcConfigurerAdapter staticResourceHandlerConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addResourceHandlers(ResourceHandlerRegistry registry) {
-                registry.addResourceHandler("classpath:/public");
-            }
-        };
-    }
-
-    @Bean
-    public CurrentTimeProvider timeProvider() {
-        return () -> Instant.now();
-    }
-
-    private static final HtmlPolicyBuilder htmlContentPolicyBuilder = new HtmlPolicyBuilder()
-            .allowUrlProtocols("http", "https", "mailto")
-            .allowAttributes("href").onElements("a")
-            .allowAttributes("src", "width", "height", "alt").onElements("img")
-            .allowAttributes("class").onElements("div", "span");
-
-    @Value("${platon.input.html_elements}")
-    private String htmlElements;
-
-    @Bean
-    public InputSanitizer htmlInputSanitizer() {
-        final String[] htmlElementList = htmlElements.split("\\s*,\\s*");
-
-        htmlContentPolicyBuilder.allowElements(htmlElementList);
-
-        final PolicyFactory htmlContentPolicy = htmlContentPolicyBuilder.toFactory();
-
-        return htmlContentPolicy::sanitize;
     }
 
 }
